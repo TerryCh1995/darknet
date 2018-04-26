969a4888a14daef35bfc46245dca95921b77aa71
TerryCh1995 <624495528@qq.com>
2018-04-27 02:58:01 +0800

    a

--- examples/detector.c
+++ examples/detector.c
@@ -627,15 +627,17 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
             DIR *dirp;
             struct dirent *dp;
             dirp = opendir(input);
+            char a[256];
             while ((dp = readdir(dirp)) != NULL){
-            	image im = load_image_color(strcat(input,dp->d_name),0,0);
+            	strcpy(a,input);
+            	image im = load_image_color(strcat(a,dp->d_name),0,0);
             	image sized = letterbox_image(im, net->w, net->h);
 	            layer l = net->layers[net->n-1];
 	            
 	            float *X = sized.data;
 	            time=what_time_is_it_now();
 	            network_predict(net, X);
-	            printf("%s: Predicted in %f seconds.\n", strcat(input,dp->d_name), what_time_is_it_now()-time);
+	            printf("%s: Predicted in %f seconds.\n", dp->d_name, what_time_is_it_now()-time);
 	            int nboxes = 0;
 	            detection *dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
 	            if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
